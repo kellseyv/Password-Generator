79f686ff3274ab34afb6e59452b9c0e195492fdd
Kellsey Vanderhoef <kellseyvanderhoef000@gmail.com>
2025-03-23 17:19:06 -0400

    Password Manager File Setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
+
+
+function generatePassword(){
+    let password1 = []
+    let password2 = []
+
+    for(let i = 0; i < 15; i++){
+        let temp = Math.floor(Math.random() * characters.length)
+        password1 += characters[temp]
+    }
+
+    firstPassword.textContent = [password1]
+    //returnPasswords(password1)
+}
+
+function returnPasswords([]){
+
+}
